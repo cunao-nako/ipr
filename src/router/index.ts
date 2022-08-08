674c9ac7c6d4c9593ff65f2f2f02d97b778a6e06
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-08 11:51:46 +0300

    router дополнен главной страницей и страницей с ошибкой

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import App from '../App.vue';
+import MainView from '../view/MainView.vue';
+import NotFoundPageView from '../view/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: App,
+      component: MainView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not found',
+      component: NotFoundPageView,
     },
   ],
 });
