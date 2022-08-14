90dec918918c8cfc40f0e14f47ec39b516f308f6
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 17:59:14 +0300

    fix(Input): дописан <style> modal

--- src/entites/input/ui/index.vue
+++ src/entites/input/ui/index.vue
@@ -5,5 +5,21 @@ const props = defineProps({
 </script>
 
 <template>
-  <input :placeholder="props.placeholder" type="text">
+  <input class="note-text" :placeholder="props.placeholder" type="text">
 </template>
+
+<style>
+.note-text {
+  width: calc(100% - 10px);
+  height: 100%;
+  margin: 5px 0 0 5px;
+  padding: 10px 10px;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  letter-spacing: 0.00625em;
+  border: 0;
+  overflow: hidden;
+  position: relative;
+}
+</style>
