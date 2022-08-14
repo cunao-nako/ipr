8b95ff28bd2bcf11e2a7d2c10071268bc19ef15b
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 23:45:38 +0300

    feat(InputsBlock): note_input

--- src/features/main/note-input/ui/InputsBlock.vue
+++ src/features/main/note-input/ui/InputsBlock.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue';
 import Input from '@/entites/input';
 import Buttons from '@/entites/buttons/ui/index.vue';
+import type { NoteInputModel } from '@/features/main/note-input/lib';
 
 const props = defineProps({
+  modelValue: Object,
   formIsVisible: Boolean,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const innerValue = ref<NoteInputModel>({
+  title: props.modelValue?.title,
+  text: props.modelValue?.text,
+});
+
+watchEffect(() => {
+  emit('update:modelValue', innerValue.value);
 });
 
 </script>
 
 <template>
-    <div class="form">
-      <Input v-if="props.formIsVisible" placeholder="Title..."/>
-      <Input placeholder="Take a note..." @click="$emit('open')"/>
-      <Buttons v-if="props.formIsVisible" @close="$emit('close')" @submit="$emit('submit')"/>
-    </div>
+  <div class="form">
+    <Input v-if="props.formIsVisible" placeholder="Title..." v-model="innerValue.title" />
+    <Input placeholder="Take a note..." v-model="innerValue.text" @click="$emit('open')" />
+    <Buttons v-if="props.formIsVisible" @close="$emit('close')" @submit="$emit('submit')" />
+  </div>
 </template>
 
 <style>
