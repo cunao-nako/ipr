2d88877c6f3a6e1ac3149eca9c1ea7ecb43d3d78
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 10:50:29 +0300

    fix(): file_structure_to_feature_sliced

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,16 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import MainView from '../view/MainView.vue';
 import NotFoundPage from '@/pages/not-found';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: MainView,
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: { template: `<h1>Hello, World!</h1>` },
+    // },
     {
       path: '/:pathMatch(.*)*',
       name: 'not found',
--- src/components/Main.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>{{ mainTitle }}</h1>
-</template>
-
-<script setup lang="ts">
-const mainTitle = 'Hello, world!';
-</script>
