fba28ac80fa731ab35472d596a3719292ba7c8a7
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 10:38:39 +0300

    fix(router): исправлен импорт  page-not-found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import MainView from '../view/MainView.vue';
-import NotFoundPageView from '../view/NotFoundView.vue';
+import NotFoundPage from '@/pages/not-found';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: '/:pathMatch(.*)*',
       name: 'not found',
-      component: NotFoundPageView,
+      component: NotFoundPage,
     },
   ],
 });
