ad9eedf42f6d4a9cb34542c453374f29d1aefd31
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 10:15:40 +0300

    feature(page-not-found-title): page-not-found

---
+++ src/entites/page-not-found-title/ui/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="page-not-found-header">
+    <h1>Page not found!</h1>
+    <RouterLink to="/">Go back</RouterLink>
+  </div>
+</template>
+
+<style>
+.page-not-found-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2em;
+}
+
+a, a:visited {
+  color: #007bff;
+}
+</style>
