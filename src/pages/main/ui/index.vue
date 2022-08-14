a186966e6936aa24315cc9a7d664ce8fd0538c1a
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 12:00:28 +0300

    feat: добавлен компонент главной страницы main_page

---
+++ src/pages/main/ui/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <img class="header-logo" src="@/shared/assets/img/logo.png" alt="logo">
+    <h2 class="header-title">Keep</h2>
+  </div>
+</template>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2em;
+}
+</style>
