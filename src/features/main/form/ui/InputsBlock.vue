d6e8bac13ec30b2c2502f695e694ed9361ec08d6
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-14 19:37:24 +0300

    feat(InputsBlock): form

---
+++ src/features/main/form/ui/InputsBlock.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Input from '@/entites/input';
+import Buttons from '@/entites/buttons/ui/index.vue';
+
+const props = defineProps({
+  formIsVisible: Boolean,
+});
+
+</script>
+
+<template>
+    <div class="form">
+      <Input v-if="props.formIsVisible" placeholder="Title..."/>
+      <Input placeholder="Take a note..." @click="$emit('open')"/>
+      <Buttons v-if="props.formIsVisible" @close="$emit('close')" @submit="$emit('submit')"/>
+    </div>
+</template>
+
+<style>
+.form {
+  position: relative;
+  border: none;
+  margin: 1px;
+  border-radius: 8px;
+  transition-duration: 0.218s;
+  transition-property: background, border, opacity, box-shadow, transform;
+  transition-timing-function: ease-in;
+}
+</style>
