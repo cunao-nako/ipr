2497c30ca088945cf2ca288d09d92344d393cecc
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-15 23:55:57 +0300

    refactor(useTodoListStore): pinia

--- src/app/stores/index.ts
+++ src/app/stores/index.ts
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia';
 import isString from 'lodash/isString';
 import isEmpty from 'lodash/isEmpty';
+import { ref } from 'vue';
 import type { newTODO, TodoList } from '@/app/stores/lib';
 
 const useTodoListStore = defineStore('todoList', () => {
-  const todoList: TodoList = [];
+  const todoList = ref<TodoList>([]);
 
   const addNewNote = ({ text, title }: newTODO) => {
-    const key = todoList.length;
-    if (isString(text) && !isEmpty(text)) {
-      todoList.push({ key, title, text });
+    const key = todoList.value.length;
+
+    const textExist = isString(text) && !isEmpty(text);
+    if (textExist) {
+      todoList.value.push({ key, title, text });
     }
   };
 
