f95be6630a6a315d1a9e1abacc3b9c3efb78fb9b
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-17 12:30:16 +0300

    fix(InputWrapper.spec.ts):  tests

--- src/entites/input-wrapper/__tests__/InputWrapper.spec.ts
+++ src/entites/input-wrapper/__tests__/InputWrapper.spec.ts
@@ -4,14 +4,12 @@ import { mount } from '@vue/test-utils';
 import InputWrapper from '@/entites/input-wrapper';
 
 describe('InputWrapper', () => {
+  const modelValue = 'modelValue';
+  const placeholder = 'placeholder';
+
+  const input = mount(InputWrapper, { props: { modelValue, placeholder } }).find('input').element;
+
   it('render with props', () => {
-    const modelValue = 'modelValue';
-    const placeholder = 'placeholder';
-
-    const wrapper = mount(InputWrapper, { props: { modelValue, placeholder } });
-
-    const input = wrapper.find('input').element;
-
     expect(input.placeholder).toBe(placeholder);
     expect(input.value).toBe(modelValue);
   });
