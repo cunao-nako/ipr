ae3c37b85262a4f1ea469003505b4490637a7625
Никонов Игорь Андреевич <inikonov@phoenixit.ru>
2022-08-18 12:13:45 +0300

    feat(HomeLink.spec.ts): обновлён снепшот компонента + добавлен тест корректной работы ссылки tests

--- src/entites/home-link/__tests__/HomeLink.spec.ts
+++ src/entites/home-link/__tests__/HomeLink.spec.ts
@@ -1,13 +1,26 @@
 import { describe, it, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { flushPromises, mount } from '@vue/test-utils';
 
 import router from '@/app/router';
 import HomeLink from '@/entites/home-link';
+
+await router.push('/forbidden');
 
 describe('HomeLink', () => {
   const wrapper = mount(HomeLink, { global: { plugins: [router] } });
 
   it('render expected ui ', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('link to home page', async () => {
+    const routerLink = wrapper.find('.home-link');
+
+    await routerLink.trigger('click');
+    await flushPromises();
+
+    const currentPath = router.currentRoute.value.path;
+
+    expect(currentPath).toBe('/');
   });
 });
